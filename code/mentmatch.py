a96a73b0cabb563e786f1e979c9a13c64dc31f5f
snazari <snazari@users.noreply.github.com>
2025-04-14 22:56:17 -0400

    summary

--- code/mentmatch.py
+++ code/mentmatch.py
@@ -5,9 +5,9 @@ import numpy as np
 import re # For cleaning text
 
 # --- Configuration ---
-MENTEE_FILE_PATH = '../data/mentee_clean.xlsx' # <--- !!! UPDATE THIS PATH !!!
-MENTOR_FILE_PATH = '../data/mentor_clean.xlsx' # <--- !!! UPDATE THIS PATH !!!
-OUTPUT_FILE_PATH = '../mentor_mentee_matches_20250402.xlsx' # Path to save the results
+MENTEE_FILE_PATH = '../data/mentee_test.xlsx' # <--- !!! UPDATE THIS PATH !!!
+MENTOR_FILE_PATH = '../data/mentor_test.xlsx' # <--- !!! UPDATE THIS PATH !!!
+OUTPUT_FILE_PATH = '../mentor_mentee_matches_20250415.xlsx' # Path to save the results
 
 # Define the TARGET standardized column names we expect to use after renaming
 # These correspond to the values in the .rename() dictionary keys used for semantic profiles
